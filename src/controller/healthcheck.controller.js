06d8cd609b54f4a46c9343c016f3f54e9af62173
Nabin-sth <nabinshrestha120@gmail.com>
2026-05-11 14:43:08 +0545

    vercel update

--- src/controller/healthcheck.controller.js
+++ src/controller/healthcheck.controller.js
@@ -1,9 +1,17 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
   console.log("Healthcheck route executed!"); // Log to confirm the function is called
-  return res.status(200).json(new ApiResponse(200, "Healthcheck passed", "ok"));
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        data: "data",
+      },
+      "ok Healthcheck passed",
+    ),
+  );
 });
 
 export default healthcheck;
